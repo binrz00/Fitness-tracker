29cd14afd7d8a213f91851e8e5920a0323b8dd8b
Brian Zauner <binrz00@hotmail.com>
2020-01-21 23:57:52 -0600

    working on the update part

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,39 +1,39 @@
 const db = require("../models");
 module.exports = function(app) {
   app.get("/api/workouts/range", function(req, res) {
-    db.Workout.find({}).then(function(results) {
-     return res.json(results);
+    db.Workout.find().sort({$natural : -1}).limit(7).then(function(results) {
+      res.json(results);
     })
     .catch(function(error){
-     return res.json(error)
+      res.json(error)
     })
   });
-
-  app.get("/api/workouts", function(req, res) {
-    db.Workout.find({}).then(function(results) {
-     return res.json(results);
-    })
-    .catch(function(error){
-     return res.json(error)
-    })
-  });
-
+  
   app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-      .then(test => {
-        res.json(test);
+    db.Workout.find().sort({$natural : -1}).limit(1).then(function(results) {
+      res.json(results);
+    })
+    .catch(function(error){
+      res.json(error)
+    })
+  });
+  
+
+  
+  app.put("/api/workouts/:id", ({ body }, res) => {
+ db.Workout.update({ id: event.target.id},{$set:{exercises:body}} , { new: true })
+      .then(result => {
+        console.log(result)
+        res.json(result);
       })
       .catch(err => {
         res.json(err);
       });
   });
-  //   app.put("/api/workouts", function(req, res) {
-  //     console.log(req.body);
-  //     db.Workout.create({
-  //       burger_name: req.body
-  //     }).then(function(data) {
-  //       res.json(data);
-  //     });
 
-  // });
+  app.post("/api/workouts",(req,res)=>{
+    db.Workout.create(req.body).then((result)=>{
+      console.log(result);
+      res.json(result)})
+  })
 };
