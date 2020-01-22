db2a5e5e097fb65bd3d1cea22d986af12dcd11be
Brian Zauner <binrz00@hotmail.com>
2020-01-21 21:21:11 -0600

    finally found my typo

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,27 +1,39 @@
 const db = require("../models");
 module.exports = function(app) {
-  app.get("/api/range", function(req, res) {
-    db.workout.findAll().then(function(results) {
-      res.json(results);
-    });
+  app.get("/api/workouts/range", function(req, res) {
+    db.Workout.find({}).then(function(results) {
+     return res.json(results);
+    })
+    .catch(function(error){
+     return res.json(error)
+    })
   });
 
-  app.post("/api/workouts", function(req, res) {
-    console.log(req.body);
-    db.workout.create({
-      burger_name: req.body
-    }).then(function(data) {
-      res.json(data);
-    });
+  app.get("/api/workouts", function(req, res) {
+    db.Workout.find({}).then(function(results) {
+     return res.json(results);
+    })
+    .catch(function(error){
+     return res.json(error)
+    })
   });
 
-    app.put("/api/workouts", function(req, res) {
-      console.log(req.body);
-      db.workout.create({
-        burger_name: req.body
-      }).then(function(data) {
-        res.json(data);
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(test => {
+        res.json(test);
+      })
+      .catch(err => {
+        res.json(err);
       });
-
   });
+  //   app.put("/api/workouts", function(req, res) {
+  //     console.log(req.body);
+  //     db.Workout.create({
+  //       burger_name: req.body
+  //     }).then(function(data) {
+  //       res.json(data);
+  //     });
+
+  // });
 };
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const db = require("./models");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(logger("dev"));
 // Static directory
 app.use(express.static("public"));
 
@@ -16,16 +17,16 @@ require("./routes/api-routes")(app);
 app.get("/", function(req, res){
   res.sendFile("index.html")
 })
-app.get("/", function(req, res){
-    res.sendFile("/index.html")
-  })
   app.get("/exercise", function (req, res) {
     res.sendfile(path.join(__dirname, "public/exercise.html"))
+  });
+  app.get("/stats", function (req, res) {
+    res.sendfile(path.join(__dirname, "public/stats.html"))
   });
 // Starts the server to begin listening
 mongoose
   .connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workoutdb",
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
     { useNewUrlParser: true }
   )
   .then(() => console.log("MongoDB successfully connected"))
